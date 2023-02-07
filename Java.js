c6942ad56d61554cdd3a38bd9c815cc57abf25d8
Harshit Sudhir Kadam <110402001+Harshit1612@users.noreply.github.com>
2023-02-07 23:10:18 +0530

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,76 @@
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement.classList.add(`form__message--${type}`);
+}
+
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+}
+
+function clearInputError(inputElement) {
+    inputElement.classList.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+    });
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        
+ 
+ 
+        var fname=document.getElementById('signupUsername').value;
+        var fname1=document.getElementById('signupUsername1').value;
+        var fpassword=document.getElementById('signuppassword').value;
+        var fpassword1=document.getElementById('signuppassword1').value;
+
+
+        if ( fname ==fname && fpassword ==fpassword )  
+     {
+        
+        setFormMessage(loginForm, "success");
+        document.getElementById("img1").hidden=false; 
+        document.getElementById("main").hidden=true; 
+
+        }
+     else{
+
+        setFormMessage(loginForm, "error", "Invalid username/password combination");
+
+
+        
+     }
+        
+    });
+
+    document.querySelectorAll(".form__input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Username must be at least 10 characters in length");
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
+    });
+});
